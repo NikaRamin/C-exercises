44ba9ff44dd5ef4cc55a68c563e7b1fd48266817
Nika Ramin <128173488+NikaRamin@users.noreply.github.com>
2023-04-26 15:50:29 +0330

    Add files via upload
    This program checks if a word is present in a string and prints the number of repetition

---
+++ word_in_string.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+
+// A program to ckeck if a word is present in a string
+
+int main(){
+
+    char str[250] , word[25];
+    int m = 0 , counter = 0 , flag = 0;
+
+    fgets(str , 250 , stdin);
+    fgets(word , 25 , stdin);
+
+    for(int i = 0 ; i < 250 ; i++){
+            if(str[i] != ' '&& str[i]!='\0'){
+                if(str[i] == word[m]){
+                    flag = 1 ;
+                    m++;
+                    continue;
+                }
+                else{
+                    m = 0;
+                    flag = 0;
+                }
+            }
+            else if(str[i] == ' '){
+                if(flag == 1){
+                    counter++;
+                }
+                m = 0;
+                flag = 0;
+            }
+            else if(str[i]=='\0'){
+                if(flag == 1){
+                    counter++;
+                }
+                break;
+            }
+    }
+   
+   if(counter==0){
+    printf("No\n");
+    printf("%d",counter);
+   }
+   else{
+    printf("Yes\n");
+    printf("%d",counter);
+   }
+
+    return 0;
+}
+

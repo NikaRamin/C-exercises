ed6d9737a40740ed108913c0b3faabb74a0ce05d
Nika Ramin <128173488+NikaRamin@users.noreply.github.com>
2023-05-25 12:38:16 +0330

    Update x_o_game.cpp

--- x_o_game.cpp
+++ x_o_game.cpp
@@ -2,57 +2,71 @@
 #include <stdlib.h>
 #include <time.h>
 
-//This program plays a random X O game, then shows the board and the winner.
-
 int main(){
     char board[3][3];
-    int rand1,w=0;
+    int rand1,w=0,turn,n=0,i,j;
     srand(time(0));
-    for(int i = 0 ; i < 3 ; i++){
-        for(int j = 0 ; j < 3 ; j++){
-            rand1 = rand() % 2 ;
-            switch(rand1){
-                case 0 : board[i][j] = 'O'; break;
-                case 1 : board[i][j] = 'X' ; break;
+    turn = rand() % 2;
+    while(1){
+        i = rand() % 3;
+        j = rand() % 3;
+        if(turn==0){
+            if(board[i][j] != 'O' && board[i][j] != 'X'){
+                board[i][j] = 'O';
+                turn = 1;
+                n++;
             }
         }
+        if(turn == 1){
+            if(board[i][j] != 'O' && board[i][j] != 'X'){
+                board[i][j] = 'X';
+                turn = 0;
+                n++;
+            }
+        }
+        if(n==9){
+            break;
+        }
     }
-    for(int i = 0 ; i < 3 ; i++){
+
+    for(i = 0 ; i < 3 ; i++){
         for(int j = 0 ; j < 3 ; j++){
             printf("%c  ",board[i][j]);
         }
         printf("\n");
     }
-        if(w == 0){
-            for(int i = 0 ; i < 3 ; i++){
-                int m = 0;
-                int n = 0;
-                if(w==1){
+
+    if(w == 0){
+        for(int i = 0 ; i < 3 ; i++){
+            int m = 0;
+            int n = 0;
+            if(w==1){
+                break;
+            }
+            for(int j = 0 ; j < 3 ; j++){
+                if(board[i][j]=='O'){
+                    m++;
+                }
+                else if(board[i][j]=='X'){
+                    n++;
+                }
+                if(m == 3 && n != 3 ){
+                    printf("O wins.\n");
+                    w = 1;
                     break;
                 }
-                for(int j = 0 ; j < 3 ; j++){
-                    if(board[i][j]=='O'){
-                        m++;
-                    }
-                    else if(board[i][j]=='X'){
-                        n++;
-                    }
-                    if(m == 3 && n != 3 ){
-                        printf("O wins.\n");
-                        w = 1;
-                        break;
-                    }
-                    if(n == 3 && m != 3 ){
-                        printf("X wins.\n");
-                        w = 1;
-                        break;
-                    }
-                    if(m==3 && n==3){
-                        w = 0;
-                    }
+                if(n == 3 && m != 3 ){
+                    printf("X wins.\n");
+                    w = 1;
+                    break;
+                }
+                if(m==3 && n==3){
+                    w = 0;
                 }
             }
         }
+    }
+
     if(w == 0){
         for(int j = 0 ; j < 3 ; j++){
             int m = 0;
@@ -149,7 +163,7 @@ int main(){
         }
     }
     if(w == 0){
-        printf("X = O");
+        printf("X = O\n");
     }
     return 0;
 }

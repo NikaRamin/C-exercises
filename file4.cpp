a47d3a2a72ef785766cebc02cda99343a5f19278
Nika Ramin <128173488+NikaRamin@users.noreply.github.com>
2023-06-02 16:34:08 +0330

    Add files via upload

---
+++ file4.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void filejoin(FILE *fp1,FILE*fp2,char str1[20],char str2[20]);
+
+
+int main(){
+char str1[20],str2[20];
+FILE *fp1,*fp2;
+filejoin(fp1,fp2,str1,str2);
+    return 0;
+}
+
+void filejoin(FILE *fp1,FILE*fp2,char str1[20],char str2[20]){
+    printf("Enter the name of file number one:\n");
+    gets(str1);
+    printf("Enter the name of file number two:\n");
+    gets(str2);
+    fp1 = fopen(str1,"r");
+    fp2 = fopen(str2,"a");
+    char c;
+    while(!feof(fp1)){
+        c = getc(fp1);
+        putc(c,fp2);
+    }
+    fclose(fp1);
+    fclose(fp2);
+    printf("DONE!");
+}

fc01d738883d5342494e8b7f27f145f16f47b36e
Nika Ramin <128173488+NikaRamin@users.noreply.github.com>
2023-06-02 16:39:26 +0330

    Add files via upload

---
+++ file5.cpp
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+void filenum(char str[20]){
+    int m = 0;
+    FILE *fp = fopen(str,"r");
+    if (fp == NULL) {
+        printf("Error: could not open file %s\n", str);
+        return;
+    }
+    while(fgetc(fp) != EOF){
+        m++;
+    }
+    fclose(fp);
+    printf("The number of Characters: %d\n",m);
+}
+
+int main(){
+    char str[20];
+    printf("Enter file name:\n");
+    scanf("%s",str);
+    filenum(str);
+    return 0;
+}
+
